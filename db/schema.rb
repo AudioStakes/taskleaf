fa39c687b58c35b65177a84f59172c076c236d14
AudioStakes <audiostakes@gmail.com>
2019-09-01 23:12:37 +0900

    Taskモデルのname属性にNOT NULL制約と30文字以内制限を追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_22_063837) do
+ActiveRecord::Schema.define(version: 2019_09_01_140453) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "tasks", force: :cascade do |t|
-    t.string "name"
+    t.string "name", limit: 30, null: false
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
