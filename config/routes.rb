5e3c70823c042f936c3042811d626a7533b96fa3
AudioStakes <audiostakes@gmail.com>
2019-09-12 22:35:07 +0900

    ルーティングの解説を追加

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,19 @@ Rails.application.routes.draw do
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
   get 'sessions/new'
+
+  # namespaceはURL改装、モジュール、URLパターン名に一括で一定の制約をかける。
+  # 以下はAdmin::UsersControllerのCRUDを、/admin/usersといったURL, admin_users_pathといったURLヘルパーメソッドとともに実現
   namespace :admin do
     resources :users
   end
   root to: 'tasks#index'
   resources :tasks
+
+  # indexだけのルート
+  # resources :tasks, only [:index]
+  #
+  # destroy, edit, update以外の４つのアクション
+  # resources :tasks, except: [:delete, :edit, :update]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
