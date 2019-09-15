0d703a7a9513327149b36a856499396a24d3fb8f
AudioStakes <audiostakes@gmail.com>
2019-09-15 22:21:10 +0900

    ログ上ではマスクしたいパラメータの設定箇所についてコメント

--- config/initializers/filter_parameter_logging.rb
+++ config/initializers/filter_parameter_logging.rb
@@ -1,4 +1,5 @@
 # Be sure to restart your server when you modify this file.
 
 # Configure sensitive parameters which will be filtered from the log file.
+# ここで指定したパラメーターはログ上では[FILTERED]と出力される
 Rails.application.config.filter_parameters += [:password]
