620d3c444608dc6fbe629462135b6fddf961577d
AudioStakes <audiostakes@gmail.com>
2019-09-18 22:47:28 +0900

    タイポ修正

--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,7 @@ Rails.application.routes.draw do
   end
   root to: 'tasks#index'
   resources :tasks do
-    post :confirm, action: :confirtm_new, on: :new
+    post :confirm, action: :confirm_new, on: :new
   end
 
   # indexだけのルート
