1b293a460983e5966c5ae9e054718b83b883e434
AudioStakes <audiostakes@gmail.com>
2019-09-26 21:42:38 +0900

    一括更新によるメモリの大量確保を避けるために1000件単位で分割して更新するように修正

--- db/migrate/20190926121934_add_finished_to_tasks.rb
+++ db/migrate/20190926121934_add_finished_to_tasks.rb
@@ -5,7 +5,7 @@ class AddFinishedToTasks < ActiveRecord::Migration[5.2]
   def up
     add_column :tasks, :finished, :boolean, null: false, default: false
 
-    Task.all.each do |task|
+    Task.find_each do |task|
       task.update(finished: true)
     end
   end
