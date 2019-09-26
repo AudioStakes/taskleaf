06850119869ea77af3343866fb41d39d83c2d7be
AudioStakes <audiostakes@gmail.com>
2019-09-26 21:47:50 +0900

    マイグレーション実行時にスキーマキャッシュが更新されるように修正

--- db/migrate/20190926121934_add_finished_to_tasks.rb
+++ db/migrate/20190926121934_add_finished_to_tasks.rb
@@ -5,6 +5,7 @@ class AddFinishedToTasks < ActiveRecord::Migration[5.2]
   def up
     add_column :tasks, :finished, :boolean, null: false, default: false
 
+    Task.reset_column_information
     Task.find_each do |task|
       task.update!(finished: true)
     end
