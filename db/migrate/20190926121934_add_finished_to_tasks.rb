97b67cf5152721a0280d085be7e3c7e7df79c96e
AudioStakes <audiostakes@gmail.com>
2019-09-26 21:29:35 +0900

    タスクの完了チェック用カラムを追加＆既存タスクはすべて完了済に更新するマイグレーションファイルを作成

---
+++ db/migrate/20190926121934_add_finished_to_tasks.rb
@@ -0,0 +1,13 @@
+class AddFinishedToTasks < ActiveRecord::Migration[5.2]
+  def up
+    add_column :tasks, :finished, :boolean, null: false, default: false
+
+    Task.all.each do |task|
+      task.update(finished: true)
+    end
+  end
+
+  def down
+    remove_column :tasks, :finished
+  end
+end
